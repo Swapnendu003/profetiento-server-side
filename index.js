6fcc4c00226097e569fff34ddc2cd9be4cf8133d
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2024-04-17 09:51:15 +0530

    modified the upskilling api

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.get("/", (req, res) => {
     }
 );
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Your Server is running");
   });
